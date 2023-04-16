5fcf6a8cc987d65e79cc21d7e4b7cecc2d298013
Fethi Hamdani <fethi.io@gmail.com>
2023-04-16 17:13:52 +0100

    completed tp

--- app/src/main/java/fethi/hamdani/assignement2/MainActivity.kt
+++ app/src/main/java/fethi/hamdani/assignement2/MainActivity.kt
@@ -1,29 +1,33 @@
 package fethi.hamdani.assignement2
 
-import android.R
+import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var button : Button
+    lateinit var input : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val intent = Intent(this, SecondPage::class.java)
         button = findViewById<Button>(R.id.button1)
+        input =  findViewById<Button>(R.id.editTextTextPersonName2)
         button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
-                val intent = Intent(this, NewActivity::class.java)
+                val message = input.text.toString()
+                intent.putExtra("message", message)
                 startActivity(intent)
             }
 
         })
+
+
+    }
 
 
 
--- app/src/main/java/fethi/hamdani/assignement2/secondPage.kt
+++ app/src/main/java/fethi/hamdani/assignement2/secondPage.kt
@@ -2,10 +2,14 @@ package fethi.hamdani.assignement2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class secondPage : AppCompatActivity() {
+class SecondPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_page)
+        val message = intent.getStringExtra("message")
+        val messageTextView: TextView = findViewById(R.id.tx3)
+        messageTextView.text = message
     }
 }
